65dc0149b09a2d2c9b1a3d88fe323356371c84e0
yuanjin zhao <zhaoy486@newschool.edu>
2013-11-06 17:35:27 -0500

    week8

---
+++ on class coed/01.php
@@ -0,0 +1,10 @@
+<！doctype html>
+<html>
+  <head>
+    <title>PHP!</title>
+  </head>
+
+  <body>
+    <>
+  </body>
+</html>
---
+++ on class coed/02.php
@@ -0,0 +1,15 @@
+<?php
+   $title = "Yay for PHP!";
+?>
+
+
+<！doctype html>
+<html>
+  <head>
+    <title>Yay for PHP!</title>
+  </head>
+
+  <body>
+    <h1><?php $title; ?></h1>
+  </body>
+</html>
